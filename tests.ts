0ca126ce7aabf7d525784628fd189b592da96f5f
DaveAtKitronik <dave.sanderson@kitronik.co.uk>
2020-02-12 14:56:55 +0000

    Fix tests.ts

--- tests.ts
+++ tests.ts
@@ -1,7 +1,7 @@
 //On detection the micro:bit screen will switch between a happy and sad face
 Kitronik_Clip_Detector.sensorSetup(Kitronik_Clip_Detector.DetectionSelection.Light)
 basic.forever(function () {
-    if (Kitronik_Clip_Detector.sensorDigitalLightDetection(Kitronik_Clip_Detector.PinSelection.P0, Kitronik_Clip_Detector.LightSelection.Dark)) {
+    if (Kitronik_Clip_Detector.sensorDigitalDetection(Kitronik_Clip_Detector.PinSelection.P0, Kitronik_Clip_Detector.LightSelection.Dark)) {
         basic.showLeds(`
             . . . . .
             . # . # .
